ebf5f26947a7f8db5f4bf62713503f5352b29441
Misael Virissimo De Moura <porincrivelqpareca@gmail.com>
2017-06-11 16:50:50 -0500

    removing newData.py

--- newData.py
+++
@@ -1,4 +0,0 @@
-from rapipd.models import Event, Group, Organizer, Location, Category
-
-e = Event(event_name="First Event", event_date="2017-08-11", event_start_time="13:58:46", event_end_time="18:58:46", event_group="ACC", event_location="My House") 
-e.save()
