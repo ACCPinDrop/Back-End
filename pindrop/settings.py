f4a34bd08f7393760173d72d8c20314a6a717cc5
Misael Virissimo De Moura <porincrivelqpareca@gmail.com>
2017-06-24 19:40:12 -0400

    Adapt the data_factory to generate N number of generic Events, Groups, Categories, Organizers and Locations

--- pindrop/settings.py
+++ pindrop/settings.py
@@ -77,10 +77,22 @@ WSGI_APPLICATION = 'pindrop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '132465',
+        'HOST': 'localhost',
+        'PORT': '',                      # Set to empty string for default.
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 
